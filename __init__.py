38b3acae23ee950a4d2895b5e512626b97e4223c
Voidy Devleoper <77110462+VoidyCD@users.noreply.github.com>
2021-02-02 09:52:41 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+EXENSTION_FILE = ".sample"  # Change it to what type of file you want to search
+
+# to make it easier to read, i put this here to show how many files i have in Python instead of .py
+EXENSTION_FILE_FULL = "Sample FILES"
+
+# CHANGE THIS TO WHERE YOU WANT TO SEARCH IN YOUR FOLDER, IF NONE, REMOVE IT FROM HERE AND FROM THE CODE
+FOLDER_TO_CHECK = "files"
+
+
+dirname = "./"
+
+
+def count_py_files_repos(dirname):
+    if os.path.exists(os.path.join(dirname, FOLDER_TO_CHECK)):
+        count = 0
+        for root, dirs, files in os.walk(dirname):
+            count += len([f for f in files if f.endswith(EXENSTION_FILE)])
+
+        # Why minus one you may ask, well, if you want to count this file has one of them, remove the minus one, in the code below
+
+        print(f"{dirname}{FOLDER_TO_CHECK} has {count-1} {EXENSTION_FILE_FULL} Files")
+
+    for name in os.listdir(dirname):
+        path = os.path.join(dirname, name)
+        if os.path.isdir(path):
+            count_py_files_repos(path)
+
+
+# PUT THE DIR NAME/WHERE TO SEARCH FOR YOUR FILES
+count_py_files_repos(dirname)
